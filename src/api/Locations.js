ed41b451631a27c94b025dbc2e6e5cfbfe13d766
Seleah O'Connor <seleah.oconnor@gmail.com>
2025-11-01 14:36:57 -0700

    syntax updates

--- src/api/Locations.js
+++ src/api/Locations.js
@@ -9,7 +9,7 @@ export const getLocations = (query) => {
 
 	let fsUrl = `https://api.foursquare.com/v2/venues/explore?query=${query}&ll=45.52345,-122.67621&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&v=${CLIENT_VERSION}`;
 
-	let fsUrl2 = '';
+	// let fsUrl2 = '';
 
 	// let fallbackLoc = locations.json();
 	let fallback = "Your search cannot be completed due to an error";
